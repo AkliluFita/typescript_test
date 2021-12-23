8ce8398ad3d043aed82d5453c052d125c9ff04c3
AkliluFita <AkliluFita@github.com>
2021-12-23 23:41:10 +0200

    First commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var fname = "aklilu";
+var message = "my name is " + fname;
+console.log(message);
+// Void
+function log(hello) {
+    console.log(hello);
+}
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+let fname: string = "aklilu"
+let message: string = `my name is ${fname}`;
+console.log(message);  
+
+
+// Void
+function log(hello: string | number): void {
+    console.log(hello)
+  }
+  
